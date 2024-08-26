57b0e3d24b78dccac1b9a64b023d1d87d25cf01c
Neelesh Mishra <neeleshmishr032@gmail.com>
2024-08-26 16:55:42 +0530

    :read elements in arraay

---
+++ c/read and print elemnts array.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+// Main function
+int main()  
+{  
+    int arr[10];  
+    int i;  
+   printf("Input 10 elements in the array :\n");  
+    for(i=0; i<10; i++)  
+    {  
+	    printf("element - %d : ",i);  
+        scanf("%d", &arr[i]);      }  
+    
+    printf("\nElements in array are: ");  
+    for(i=0; i<10; i++)  
+    {  
+        printf("%d  ", arr[i]);  // Print each element in the array
+    } 
+    printf("\n");
+	return 0;	
+}
