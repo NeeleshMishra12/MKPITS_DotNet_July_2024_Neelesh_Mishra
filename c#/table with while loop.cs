e4e7b767c7db5baa6fb0f10a1d8d631e3dbb9724
Neelesh Mishra <neeleshmishr032@gmail.com>
2024-08-29 10:43:15 +0530

    print a tble with while loop

---
+++ c#/table with while loop.cs
@@ -0,0 +1,22 @@
+using System;
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+       
+           int i = 10;
+            do
+            {
+                Console.WriteLine(i);
+                i=i+10;
+               
+            }
+
+             while(i<=100);
+
+            Console.Read();
+            
+
+    }
+}
