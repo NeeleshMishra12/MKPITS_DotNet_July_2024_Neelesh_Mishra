6e2fefbe20c895495c9adf2afa0114168184adbd
Neelesh Mishra <neeleshmishr032@gmail.com>
2024-10-21 11:04:27 +0530

    creating a method\

---
+++ c#/creating a method.cs
@@ -0,0 +1,16 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int result = Add(5, 7);
+        Console.WriteLine("Sum: " + result);
+    }
+
+    static int Add(int a, int b)
+    {
+        return a + b;
+    }
+   Console.Read();
+}
