9b15d5b5f4a1a4e0bd55908b2ad5597f35b48676
Neelesh Mishra <neeleshmishr032@gmail.com>
2024-10-29 11:20:33 +0530

    check if a character is vowel

---
+++ java/check if a character is a vowl.java
@@ -0,0 +1,10 @@
+public class VowelCheck {
+    public static void main(String[] args) {
+        char ch = 'a';
+        if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+            System.out.println("The character is a vowel.");
+        } else {
+            System.out.println("The character is a consonant.");
+        }
+    }
+}
