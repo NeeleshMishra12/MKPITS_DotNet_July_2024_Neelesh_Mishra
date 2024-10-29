202ac0f93b906360370530b8e19fa97cfba937d5
Neelesh Mishra <neeleshmishr032@gmail.com>
2024-10-29 11:19:20 +0530

    check number is visible by 3 or 5

---
+++ java/check number is visible by 3 or 5.java
@@ -0,0 +1,10 @@
+public class DivisibilityCheck {
+    public static void main(String[] args) {
+        int num = 15;
+        if (num % 3 == 0 && num % 5 == 0) {
+            System.out.println("The number is divisible by both 3 and 5.");
+        } else {
+            System.out.println("The number is not divisible by both 3 and 5.");
+        }
+    }
+}
