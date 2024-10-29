3aca6ada61fadca581c1b92be17ef7da45aa39c9
Neelesh Mishra <neeleshmishr032@gmail.com>
2024-10-29 11:12:53 +0530

    check if a string is empty

---
+++ java/check if a string is empty.java
@@ -0,0 +1,10 @@
+public class StringEmptyCheck {
+    public static void main(String[] args) {
+        String text = "";
+        if (text.isEmpty()) {
+            System.out.println("The string is empty.");
+        } else {
+            System.out.println("The string is not empty.");
+        }
+    }
+}
