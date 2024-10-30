4209ee4cf58fd60e47a243fd0fd1c666ee529181
Neelesh Mishra <neeleshmishr032@gmail.com>
2024-10-30 21:49:12 +0530

    find second largest element in array

---
+++ c/find second largest element .c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main() {
+    int n;
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+    int arr[n];
+    printf("Enter the elements:\n");
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    int max = arr[0], second_max = -1;
+    for (int i = 1; i < n; i++) {
+        if (arr[i] > max) {
+            second_max = max;
+            max = arr[i];
+        } else if (arr[i] > second_max && arr[i] < max) {
+            second_max = arr[i];
+        }
+    }
+    printf("Second largest element: %d", second_max);
+    return 0;
+}
